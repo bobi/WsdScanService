7b902fd980790cfb6cbb7179db1362f34e241fb5
bobi <andrey.bardashevsky@gmail.com>
2026-03-02 20:23:46 -0500

    Refactor Sane configuration and scanning logic to support image transformation and improve clarity

--- WsdScanService.Common/Configuration/SaneConfiguration.cs
+++ WsdScanService.Common/Configuration/SaneConfiguration.cs
@@ -1,5 +1,12 @@
 namespace WsdScanService.Common.Configuration;
 
+public record ImageConverterConfiguration
+{
+    public string? Path { get; init; }
+
+    public ICollection<string>? Args { get; init; }
+}
+
 public record SaneConfiguration
 {
     public bool UseSaneBackend { get; init; } = false;
@@ -9,4 +16,8 @@ public record SaneConfiguration
     public string? BackendPath { get; init; }
 
     public ICollection<string>? AdditionalArgs { get; init; }
+
+    public string? Format { get; init; }
+
+    public ImageConverterConfiguration? ImageConverter { get; init; }
 }
